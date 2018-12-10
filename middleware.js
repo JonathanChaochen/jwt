0d9c47b56a16aa3d26d6dd7d64d2ff5aca8d5059
chao chen <chaochen42@gmail.com>
2018-12-10 22:16:35 +1300

    init

---
+++ middleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+
+const checkToken = (req, res) => {
+  const token = req.header['x-access-token'] || req.headers['authorization'];
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
+  if (token) {
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
+    });
+  }
+};
+
+module.exports = {
+  checkToken
+};
